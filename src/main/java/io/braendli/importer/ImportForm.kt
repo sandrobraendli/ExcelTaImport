ff6f115afa2a4505b9d99a4f7954387d2222f38a
Sandro Brändli <sandro.braendli@gmail.com>
2018-01-30 08:50:09 +0100

    Convert to kotlin

---
+++ src/main/java/io/braendli/importer/ImportForm.kt
@@ -0,0 +1,110 @@
+package io.braendli.importer
+
+import javafx.application.Application
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.CheckBox
+import javafx.scene.control.TextField
+import javafx.scene.image.Image
+import javafx.scene.layout.GridPane
+import javafx.stage.FileChooser
+import javafx.stage.FileChooser.ExtensionFilter
+import javafx.stage.Stage
+import org.controlsfx.control.NotificationPane
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.io.File
+import java.io.IOException
+
+class ImportForm : Application() {
+    private lateinit var stage: Stage
+    private lateinit var notification: NotificationPane
+    @FXML
+    private lateinit var deleteOldDataBox: CheckBox
+    @FXML
+    private lateinit var importButton: Button
+    @FXML
+    private lateinit var excelField: TextField
+    @FXML
+    private lateinit var databaseField: TextField
+
+    @FXML
+    fun handleChooseExcel() {
+        val fileChooser = FileChooser()
+        fileChooser.title = "Excel Datei wählen"
+        fileChooser.extensionFilters.add(ExtensionFilter("Excel Dateien", "*.xlsx"))
+        updateTextField(excelField, fileChooser.showOpenDialog(stage))
+    }
+
+    @FXML
+    fun handleChooseDatabase() {
+        val fileChooser = FileChooser()
+        fileChooser.title = "Datenbank wählen"
+        fileChooser.extensionFilters.add(ExtensionFilter("Datenbanken", "*.FDB"))
+        updateTextField(databaseField, fileChooser.showOpenDialog(stage))
+    }
+
+    private fun updateTextField(field: TextField?, file: File?) {
+        if (file != null && file.exists()) {
+            field!!.text = file.absolutePath
+        }
+    }
+
+    @FXML
+    fun initialize() {
+        updateTextField(excelField, File(System.getProperty("user.home"), "Desktop\\users.xlsx"))
+        updateTextField(excelField, File("users.xlsx"))
+        updateTextField(databaseField, File("C:\\Program Files\\SafeScan\\TA\\TADATA.FDB"))
+        importButton.disableProperty().bind(excelField.textProperty().isEmpty.or(databaseField.textProperty().isEmpty))
+    }
+
+    @Throws(IOException::class)
+    override fun start(stage: Stage) {
+        val loader = FXMLLoader(javaClass.getResource("/import_form.fxml"))
+        val root = loader.load<Parent>()
+        val notification = NotificationPane(root)
+        (loader.getController<Any>() as ImportForm).setUp(stage, notification)
+        val scene = Scene(notification, 500.0, -1.0)
+        stage.icons.add(Image(javaClass.getResourceAsStream("/icons/app.png")))
+        stage.title = "Excel TA Import"
+        stage.scene = scene
+        stage.show()
+    }
+
+    private fun setUp(stage: Stage, notification: NotificationPane) {
+        this.stage = stage
+        this.notification = notification
+    }
+
+    fun importData() {
+        LOG.info("Starting import")
+        try {
+            Importer.importToDatabase(
+                    deleteOldDataBox.isSelected,
+                    File(excelField.text),
+                    File(databaseField.text)
+            )
+            notification.text = "Import erfolgreich abgeschlossen"
+            LOG.info("Import successful")
+        } catch (e: Exception) {
+            notification.text = "Der Import ist fehlgeschlagen"
+            LOG.error("Import failed", e)
+        } finally {
+            notification.show()
+        }
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ImportForm::class.java)
+
+        @Throws(ClassNotFoundException::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(*args)
+        }
+    }
+}
---
+++ src/main/java/io/braendli/importer/Importer.kt
@@ -0,0 +1,105 @@
+package io.braendli.importer
+
+import org.apache.poi.ss.usermodel.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.lang.reflect.Field
+import java.sql.*
+
+import java.util.stream.Collectors.toList
+import java.util.stream.StreamSupport.stream
+
+object Importer {
+    private val LOG = LoggerFactory.getLogger(Importer::class.java)
+
+    init {
+        try {
+            System.setProperty("java.library.path", File("lib").absolutePath)
+            val sysPaths = ClassLoader::class.java.getDeclaredField("sys_paths")
+            sysPaths.isAccessible = true
+            sysPaths.set(null, null)
+            Class.forName("org.firebirdsql.gds.impl.jni.JniGDSImpl")
+            LOG.debug("Native library loaded")
+        } catch (e: Exception) {
+            LOG.error("Loading native library failed", e)
+        }
+    }
+
+    @Throws(Exception::class)
+    fun importToDatabase(deleteOldData: Boolean, excelFile: File, databaseFile: File) {
+        getDatabaseConnection(databaseFile).use { con ->
+            LOG.info("Start reading exel file: {}", excelFile)
+            val cells = readExcel(excelFile)
+
+            if (deleteOldData) {
+                LOG.info("Deleting old data")
+                deleteOldData(con)
+            }
+            LOG.info("Importing into database")
+            insertData(cells, con)
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun deleteOldData(con: Connection) {
+        con.createStatement().use { stmt ->
+            stmt.execute("delete from attendant where userid >= 100")
+            stmt.execute("delete from users where id >= 100")
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun getDatabaseConnection(databaseFile: File): Connection {
+        val conString = String.format("jdbc:firebirdsql:embedded:%s?encoding=NONE", databaseFile.absolutePath)
+        LOG.info("Connecting to database: {}", conString)
+        return DriverManager.getConnection(conString, "SYSDBA", "a")
+    }
+
+    @Throws(Exception::class)
+    private fun readExcel(excelFile: File): List<List<Cell>> {
+        FileInputStream(excelFile).use { inp ->
+            val wb = WorkbookFactory.create(inp)
+            val sheet = wb.getSheetAt(0)
+
+            return stream<Row>(sheet.spliterator(), false).skip(1)
+                    .map { r -> stream<Cell>(r.spliterator(), false).collect(toList()) }
+                    .filter { l -> l.size >= 3 && !l.contains(null) }
+                    .collect(toList())
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun insertData(list: List<List<Cell>>, con: Connection) {
+        val insertString = "INSERT INTO USERS(ID, USERNAME, FIRSTNAME, LASTNAME, IDCARD) VALUES(?, ?, ?, ?, ?)"
+        var id = getStartId(con)
+
+        con.prepareStatement(insertString).use { insertStmt ->
+            for (row in list) {
+                LOG.debug("Importing user: {}", row)
+                var pos = 1
+                insertStmt.setInt(pos++, id)
+                insertStmt.setInt(pos++, id)
+                insertStmt.setString(pos++, row[pos - 4].stringCellValue)
+                insertStmt.setString(pos++, row[pos - 4].stringCellValue)
+                insertStmt.setInt(pos++, row[pos - 4].numericCellValue.toInt())
+                insertStmt.execute()
+                id++
+            }
+        }
+    }
+
+    @Throws(SQLException::class)
+    private fun getStartId(con: Connection): Int {
+        con.createStatement().use { stmt ->
+            stmt.executeQuery("select max(id) from users").use { rs ->
+                rs.next()
+                val id = rs.getInt(1)
+                return if (id < 100) 100 else id + 1
+            }
+        }
+    }
+}
