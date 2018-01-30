0fcec28583fb2f6ea9a605c61793ee3853bb2f5d
Sandro Brändli <sandro.braendli@gmail.com>
2018-01-30 13:03:49 +0000

    Convert build to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.2.21"
+    id("edu.sc.seis.launch4j") version "2.3.0"
+}
+
+version = "1.0"
+
+launch4j {
+    mainClassName = "io.braendli.importer.ImportForm"
+    icon = "../../src/main/resources/icons/app.ico"
+    chdir = "."
+}
+
+dependencies {
+    compile("org.apache.poi:poi:3.15")
+    compile("org.apache.poi:poi-ooxml:3.15")
+
+    compile("org.firebirdsql.jdbc:jaybird-jdk17:2.2.10")
+
+    compile("org.controlsfx:controlsfx:8.40.12")
+
+    compile("org.slf4j:slf4j-api:1.7.22")
+    compile("ch.qos.logback:logback-classic:1.1.9")
+
+    compile(files("lib/jaybird22.dll"))
+    compile(files("lib/jaybird22_x64.dll"))
+    compile(files("lib/fbembed.dll"))
+    compile(kotlin("stdlib"))
+}
+
+repositories {
+    mavenCentral()
+}
