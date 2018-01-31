e73ff83acdb7443323a4658b994e7bd96c3c2970
Sandro Brändli <sandro.braendli@gmail.com>
2018-01-31 08:37:01 +0100

    Remove unused import

--- src/main/java/io/braendli/importer/Importer.kt
+++ src/main/java/io/braendli/importer/Importer.kt
@@ -8,7 +8,6 @@ import java.io.File
 import java.io.FileInputStream
 import java.sql.Connection
 import java.sql.DriverManager
-import java.sql.SQLException
 import java.util.stream.Collectors.toList
 import java.util.stream.StreamSupport.stream
 
